99ab071c242777cf41b7cd823792e055cd7ab0a8
Gustav <31858743+GustavCleveman@users.noreply.github.com>
2019-04-24 09:32:51 +0200

    asdfasdfasdf

--- azureInstallWebServer.ps1
+++ azureInstallWebServer.ps1
@@ -18,3 +18,20 @@ SampleIISInstall
 
 Start-DscConfiguration -Path .\SampleIISInstall -Wait 
 
+#WinRm ssc on the target VM
+#Create cert and store cert and thumbprint in variables
+$CertName = '52.236.59.127'
+$Cert = New-SelfSignedCertificate -certstorelocation cert:\localmachine\my -dnsname $Certname
+$Thumbprint = $Cert.Thumbprint
+
+#Open firewall port on the target VM
+New-NetFirewallRule -DisplayName 'WinRm (HTTPS-In)' -Name 'WinRm (HTTPS-In)' -Profile any -LocalPort 5986 -Protocol TCP
+
+#Set winRm to listen to Https with the cert thumbprint
+New-Item -Path WSMan:\LocalHost\Listener -Transport HTTPS -Address * -CertificateThumbPrint $Thumbprint –Force
+
+# Remove HTTP listener (optional)
+Winrm enumerate winrm/config/listener
+Get-ChildItem WSMan:\Localhost\listener | Where -Property Keys -eq 'Transport=HTTP' | Remove-Item -Recurse
+
+$Cert | Export-Certificate -FilePath "$env:temp\DscPublicKey.cer" -Force
